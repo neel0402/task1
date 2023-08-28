86c88d510843f1c27ea25a283e32cbea867d50aa
neel0402 <122443158+neel0402@users.noreply.github.com>
2023-08-28 14:54:37 +0530

    Add files via upload

---
+++ assighment.js
@@ -0,0 +1,59 @@
+// // -> give the list which variable name not works in js
+// let "a"=0
+// let 'a'=0
+// let `10`=0
+// let 1=0
+
+
+// -----------------------------------------------------------------------------
+
+// let x = 100
+// var y = 100
+// const z = 100
+// const m
+// {
+//       x= 90
+//       y= 90
+//       z= 90
+//       console.log(x) _____90______________ point-x1
+//       console.log(y) _____90_____________ point-y1
+//       console.log(z) _____90______________ point-z1
+//       console.log(m) _____Missing initializer in const declaration______________ 
+// } 
+
+// console.log(x) __100_________________ point-x2
+// console.log(y) __100_________________ point-y2
+// console.log(z) __100_________________ point-z2
+
+// what will print at x1-__ x1 is not defined_____
+// what will print at y1-__90____
+// what will print at z1-___z1 is not defined____
+// what will print at x2-___100____
+// what will print at y2-___100____
+// what will print at z2-___100____
+
+// -------------------------------------------------------------------------------
+
+// -> task-2
+
+// console.log(x) ____x is not defined_______________ point-x2
+// console.log(y) ____100______________ point-y2
+// console.log(z) ____z is not defined_______________ point-z2
+
+// {
+//       let x = 100
+//       var y = 100
+//       const z = 100
+// }
+
+// console.log(x) _____x is not defined______________ point-x1
+// console.log(y) _____100______________ point-y1
+// console.log(z) _____z is not defined______________ point-z1
+
+
+// what will print at x1-__x is not defined_____
+// what will print at y1-__y is not defined_____
+// what will print at z1-__100_____
+// what will print at x2-__x2 is not defined_________
+// what will print at y2-___100____
+// what will print at z2-__z2 is not defined_____
---
+++ familytree1.js
@@ -0,0 +1,70 @@
+let grandfather={
+    name:"nanubhai",
+    age:71,
+    son1:[
+        {
+            name:"prafulbhai",
+            age:45,
+            child1:
+            {
+                name:"bhavy",
+                age:18,
+                std: "bca running",
+            },
+            child2:
+            {
+                name:"dharmik",
+                age:15,
+                std: "10th",
+            },
+            vehicle:
+            {
+                model:"i20",
+                color:"red",
+                price:"10,00,000",
+            }
+        },
+    ],
+    son2:[
+        {
+            name:"pareshbhai",
+            age:43,
+            child1:
+            {
+                name:"mansi",
+                age:22,
+                job:"jwellary designer",
+            },
+            child2:
+            {
+                name:"neel",
+                age:19,
+                std:"bca running",
+            },
+            vehicle:
+            {
+                model:"XUV 700",
+                color:"dark blue",
+                price:"17,00,000",
+            }
+        },
+    ],
+    son3:[
+        {
+            name:"rajubhai",
+            age:41,
+            child1:
+            {
+                name:"tulsi",
+                age:10,
+                std:"senior k.g",
+            },
+            vehicle:
+            {
+                model:"i10 grand",
+                color:"white",
+                price:"6,00,000",
+            }
+        },
+    ],
+}
