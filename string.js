a783f3a4ba4dddb3326430e3f1ac3911a6dc4697
neel0402 <122443158+neel0402@users.noreply.github.com>
2023-09-11 15:02:22 +0530

    Add files via upload

--- string.js
+++ string.js
@@ -35,7 +35,7 @@ console.log("mapmethod:", mapmethod)
 // forloop method 
 
 let formethod = arr.forEach((e) => {
-    console.log("🚀 ~ file: string.js:40 ~ for ~ for:", e)
+    console.log(" ~ for arr ~ for:", e)
 })
 
 
@@ -101,3 +101,94 @@ console.log("assending order:", arr)
 let b = (a, b) => b - a
 arr.sort(b)
 console.log("assending order:", arr)
+
+// 12. reverce an array with use of method
+
+arr.reverse()
+console.log("~ arr:", arr)
+
+// 13. reverce an array with use of for-loop
+
+let reversed_array = []
+for (let i = arr.length - 1; i >= 0; i--) {
+  reversed_array.push(arr[i]);
+}
+
+console.log("~ reversed_array:", reversed_array)
+
+// [1,2,3] => [3,2,1] [2,1]
+
+// =========================================================
+
+// task-2
+let Arr = [1, 2, 3, 6, 7, 9, 3, 56, 8]
+
+// 1. give all value which can devided by 2 - not use filter
+for (let i = 0; i < Arr.length; i++) {
+  if (Arr[i] % 2 == 0) {
+    console.log(" ~ i arr:", Arr[i])
+  }
+}
+
+// 2. give all value above 3 by use of filter
+
+let FillArr = Arr.filter((e) => {
+  return e > 3
+})
+console.log("FillArr:", FillArr)
+
+// 3. give index of 56
+
+let FindArr = Arr.findIndex((e) => {
+  return e == 56
+})
+console.log("FindArr:", FindArr)
+
+// 4. give true/false result when check given value is exists in array or not
+
+let check = arr.map((e) => {
+  return e == 7
+})
+console.log("~ check:", check)
+
+// ========================================================
+// task-3
+
+// 1. ['i','am', 'urvish'] ==> want ['urvish','am','i']
+
+
+let str = ['i', 'am', 'urvish']
+
+//Revres a string by loop
+
+let reversed_String = []
+for (let i = str.length - 1; i >= 0; i--) {
+  reversed_String.push(str[i]);
+}
+console.log("🚀 ~ file: task3.js:137 ~ reversed_String:", reversed_String)
+
+// by map
+let reversed_String_1 = str.map((str) => {
+  return str; // or any logic you want to perform
+}).reverse();
+console.log("🚀 ~ file: string.js:174 ~ reversed_String_1:", reversed_String_1)
+
+
+// 2. want "i am urvish" without use join method
+
+
+let ToString = str.toString().replace(/\,/g, " ")
+console.log(" ToString:", ToString)
+/*
+
+=========================================================
+task-4
+
+['i','am', 'urvish'] ==> want ['hsivru','ma','i']
+by use of loop
+*/
+let Name = ['i', 'am', 'urvish']
+for (let i = Name.length - 1; i >= 0; i--) {
+  let ame = Name[i].split(" ")
+  console.log("🚀 ~ file: task3.js:183 ~ Name:", ame)
+}
